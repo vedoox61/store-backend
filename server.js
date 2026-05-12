fc9a5aaa3f07f0634860d921595c36dc3c677c98
vedoox <vedoox@vedoox.vedoox>
2026-05-12 14:45:15 +0100

    fix: listen on 0.0.0.0

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
     res.json({ message: '✅ Brahim Store API is running!' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
